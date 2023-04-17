29998375ad2db452145813585af016dfd805a733
naga_perla <hemanth.vja@gmail.com>
2023-04-17 20:36:45 +0100

    hotfix

--- be/src/services/question.service.ts
+++ be/src/services/question.service.ts
@@ -13,6 +13,8 @@ export class QuestionService {
   async getRandomQuestions(): Promise<Question[]> {
     const questions = await this.questionRepository.find();
 
+    console.log("all questions - comes from database", questions);
+
     // Group questions by category
     const grouped = questions.reduce((acc, curr) => {
       if (!acc[curr.category]) {
@@ -21,6 +23,8 @@ export class QuestionService {
       acc[curr.category].push(curr);
       return acc;
     }, {});
+    console.log("questions has been grouped by category", grouped);
+
 
     // Pick 1 random question from each category
     const selected = Object.keys(grouped).reduce((acc, curr) => {
@@ -29,16 +33,22 @@ export class QuestionService {
       acc.push(questions[randomIndex]);
       return acc;
     }, []);
+    console.log("pick 1 question from each category", selected);
+
 
     // Pick additional random questions until we have 5 in total
     while (selected.length < 5) {
-      const randomIndex = Math.floor(Math.random() * data.length);
-      const randomQuestion = data[randomIndex];
-      if (!selected.includes(randomQuestion) && !randomQuestion.isRedundant) {
+      const randomIndex = Math.floor(Math.random() * questions.length);
+      const randomQuestion = questions[randomIndex];
+      if (!selected.includes(randomQuestion) && !randomQuestion.category) {
         selected.push(randomQuestion);
       }
     }
 
-    return selected;
+    const finalFive = selected.splice(0, 5);
+    console.log("finalFive", finalFive);
+
+
+    return finalFive; // 5 random questions from selected list
   }
 }
